d3ea406fc95df01ff476404b839222adfd8dec94
Petr <kod.rf@yandex.ru>
2024-09-04 11:52:34 -0400

    Initial Commit

---
+++ Counter1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter1
+//
+//  Created by P on 04/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
